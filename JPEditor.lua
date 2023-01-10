e63af41705eae31369a6feb3e37e5815aabd4d22
Oraite <114744433+Oraite@users.noreply.github.com>
2023-01-10 00:08:19 +0000

    Update JPEditor.lua

--- JPEditor.lua
+++ JPEditor.lua
@@ -18,7 +18,8 @@
     local debug_enabled <const> = true
 
     --- Script wide Variables
-    local script_local_version = '0.1.165'
+    local script_local_version = '0.1.170'
+    local script_current_version = nil
     local finished_asynchronous_init = false
 
     --- Script wide Tables
@@ -127,11 +128,32 @@
             end
         
             -- Here there's a new version
+            local found_string_start_index = string.find(p_body, 'script_local_version = \'')
+            local found_string_lenght = string.len('script_local_version = \'')
+            local small_body = string.sub(p_body, 0, found_string_start_index + 100)
+            local string_start_index = found_string_start_index + found_string_lenght
+            local string_end_index = 0
+            local while_counter = string_start_index
+
+            while (true) do
+                util.yield()
+
+                if (string.sub(small_body, while_counter, while_counter) == '\'') then
+                    break
+                end
+
+                string_end_index = while_counter
+                while_counter += 1
+            end
+
+            script_current_version = string.sub(small_body, string_start_index, string_end_index)
+            util.log('#1501, script_current_version == ' .. script_current_version)
+
             -- Create update option / action
             menu.my_root():action('Update Script', {''}, '', |p_click_type, p_effective_issuer| -> update_script(), nil, nil, COMMANDPERM_USERONLY)
             notification('[ JPEDITOR ]\nThere\'s a new version available.\nUpdate the script to get the newest version.', 3, '[ JPEDITOR ] There\'s a new version available. Update the script to get the newest version.')
             finished_asynchronous_init = true
-            
+
             return
         end)
     
@@ -159,28 +181,6 @@
     function (param_click_type, param_effective_issuer) 
         util.restart_script()
     end, nil, nil, COMMANDPERM_USERONLY)
-
-    if (script_has_new_verion) then 
-        menu.my_root():action('Update Script', {''}, '', 
-        function (param_click_type, param_effective_issuer) 
-            async_http.init('raw.githubusercontent.com','/Oraite/JPEditor/main/JPEditor.lua', function (p_body, p_header_fields, p_status_code) 
-                if select(2, load(p_body)) then
-                    notification('[ JPEDITOR ]\nScript download has failed.\nPlease try again later.\nIf this continues to happen then manually update via github.', 3, '[ JPEDITOR ] Script download has failed. Please try again later. If this continues to happen then manually update via github.') 
-                    return
-                end
-
-                local file = io.open(filesystem.scripts_dir() .. SCRIPT_RELPATH, 'wb')
-                file:write(p_body)
-                file:close()
-
-                notification('[ JPEDITOR ]\nSuccessfully updated.\nRestarting script...', 3, '[ JPEDITOR ] Successfully updated.') 
-                util.restart_script()
-            end)
-
-            -- Dispatch
-            async_http.dispatch()
-        end)
-    end
 
     menu.my_root():divider('JPEditor')
 
